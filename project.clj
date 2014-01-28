1ff21adf7d59a1ff8d490c303fd1ae856d35f7ee
Trevor Hartman <trevorhartman@gmail.com>
2014-01-28 08:45:28 -0700

    Add deploy config

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject lein-worker "0.1.0"
   :description "A plugin to upload worker jars to Iron Worker"
   :url "https://github.com/devth/lein-worker"
+  :lein-release {:deploy-via :clojars}
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
